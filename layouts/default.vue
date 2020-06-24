00044123566857f51f2acacaa1b85e5a07845391
Kohei.Yaginuma <f.kohei.yaginuma@gmail.com>
2020-06-24 17:42:49 +0900

    [#4]LIFFの導入

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,23 +8,33 @@
 <script>
 import Search from "~/components/Search";
 export default {
-  components:{
-    Search,
+  components: {
+    Search
+  },
+  mounted() {
+    // LIFFの初期化
+    liff
+      .init({
+        liffId: process.env.LIFF_ID
+      })
+      .then(() => {
+        // Webブラウザからアクセスされた場合は、LINEにログインする
+        if (!liff.isInClient() && !liff.isLoggedIn()) {
+          window.alert("LINEアカウントにログインしてください。")
+          liff.login({ redirectUri: location.href })
+        }
+      })
+      .catch(err => {
+        console.log("LIFF Initialization failed ", err)
+      })
   }
-}
+};
 </script>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,13 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Resto Clip',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '飲食店をお気に入りできるLINEミニアプリ' }
     ],
+    script: [{ src: 'https://static.line-scdn.net/liff/edge/2.1/sdk.js' }],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
