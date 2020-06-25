834649b16060f2dc9849b54051533d10d829572c
Kohei.Yaginuma <f.kohei.yaginuma@gmail.com>
2020-06-25 16:56:04 +0900

    [#5]不要な記述の削除

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,13 +2,9 @@
 </template>
 
 <script>
-import Search from "~/components/Search";
 export default {
-  components: {
-    Search
-  },
   mounted() {
-    this.$store.dispatch("login");
+    this.$store.dispatch("login")
   }
 };
 </script>
